0abe00f2b633419462675c4f3eb6abacd5fc1cdc
learneeer <learneeer.9@gmail.com>
2019-05-06 12:18:14 +0200

    tab & header

--- main.js
+++ main.js
@@ -3,16 +3,51 @@ var start_open = function (name) {
 
 
     content = document.getElementsByClassName("content");
+
     for (i = 0; i < content.length; i++) {
         content[i].style.display = "none";
     }
+
     document.getElementById(name).style.display = "block";
 
 
+
     links = document.getElementsByClassName("links");
+
     for (i = 0; i < links.length; i++) {
         links[i].className = links[i].className.replace(" active", "");
     }
-    event.currentTarget.className += " active";
 
+    event.currentTarget.className += " active";
+}
+
+
+
+var start_click = function () {
+    document.querySelector('.tab').classList.toggle('active');
+    document.querySelector('.links1').className += " active";
+
+    document.querySelector('.name').classList.toggle('opacity0');
+    document.querySelector('.button_area').classList.toggle('opacity0');
+    document.querySelector('.burger').classList.toggle('opacity0');
+
+    document.getElementById('our_offer').style.display = "block";
+}
+
+var end_click = function () {
+    document.querySelector('.tab').classList.toggle('active');
+    document.getElementById('our_offer').style.display = "none";
+    document.getElementById('about_us').style.display = "none";
+    document.getElementById('contact').style.display = "none";
+
+    document.querySelector('.name').classList.toggle('opacity0');
+    document.querySelector('.button_area').classList.toggle('opacity0');
+    document.querySelector('.burger').classList.toggle('opacity0');
+
+
+    links = document.getElementsByClassName("links");
+
+    for (i = 0; i < links.length; i++) {
+        links[i].className = links[i].className.replace(" active", "");
+    }
 }
